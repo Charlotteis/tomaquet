cea1af847a325c8d18f65867107b4b6e57b9939d
System <none>
2017-03-20 16:34:28 +0000

    ⚡️👸 feat: footer component and cuter css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import Header from './components/Header'
 import Pomo from './components/Pomo'
+import Footer from './components/Footer'
 
 class App extends Component {
   render () {
     return (
       <div>
         <Header />
-        <Pomo />
+        <main role='main'>
+          <Pomo />
+        </main>
+        <Footer />
       </div>
     )
   }
