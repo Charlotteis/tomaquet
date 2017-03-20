fbe0b5bb5eecd7120747cedf81bab038b17157e5
System <none>
2017-03-20 13:11:47 +0000

    💠💺 feat: header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
+import Header from './components/Header'
+import './'
 
 class App extends Component {
   render () {
     return (
       <div>
-        <header>
-          <h1>Tomáquet - A pomodoro tracker</h1>
-        </header>
+        <Header />
       </div>
     )
   }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Header.css'
+
+class Header extends Component {
+  render () {
+    return (
+      <header>
+        <h1 className='title'>&#x1f345; Tomáquet - A pomodoro tracker &#x1f345;</h1>
+      </header>
+    )
+  }
+}
+
+export default Header
