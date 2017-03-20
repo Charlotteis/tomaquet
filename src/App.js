b05f3519ffeee5c2d0a96185ea9779ff2b0378f0
System <none>
2017-03-20 14:17:28 +0000

    🏉🐷 feat: basic timer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import Header from './components/Header'
-import './'
+import Pomo from './components/Pomo'
 
 class App extends Component {
   render () {
     return (
       <div>
         <Header />
+        <Pomo />
       </div>
     )
   }
